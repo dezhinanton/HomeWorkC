90f21c9926e6edbaa101616ad2d681f7c0319566
Dezhin Anton <dezhin@email.com>
2023-03-14 14:48:04 +0300

    Task10 Done

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 100)
+{
+    Console.Write("Вы ошиблись!\nВведите число(от 100 до 999): ");
+    n = int.Parse(Console.ReadLine()!);
+}
+int n1 = n / 10;
+int n2 = n1 % 10;
+Console.WriteLine(n2);
