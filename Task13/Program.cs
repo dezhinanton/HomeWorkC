f6172056115eb907d5e9e34297cd4bb89d79dce3
Dezhin Anton <dezhin@email.com>
2023-03-14 15:45:19 +0300

    Task13 Done

---
+++ Task13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+while(n < 100)
+{    Console.WriteLine("Ошибка! Введите трех- и более значное число : ");
+    n = int.Parse(Console.ReadLine()!);
+}              
+while(n > 1000)    
+    n = n/10;
+    Console.WriteLine(n%10); 
