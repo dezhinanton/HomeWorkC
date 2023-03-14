d68bb791c264f7b817a62931e88f3533267ad02b
Dezhin Anton <dezhin@email.com>
2023-03-14 16:05:55 +0300

    Task15 Done

---
+++ Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int n = int.Parse(Console.ReadLine()!);
+while(n < 1 || n > 7)
+{
+    Console.WriteLine("ОШИБКА!Введите число от 1 до 7: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+if(n == 6 || n == 7)
+    Console.WriteLine("Да, это выходной день!");
+else
+    Console.WriteLine("Нет, это будний день!");      
+        
