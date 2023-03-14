f7cd589af26103bd82deea0870e1860962e9c2b6
Dezhin Anton <dezhin@email.com>
2023-03-14 10:44:25 +0300

    Task4 Done

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3-е число: ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
