706d61158d3786cc920a31995d44a7cb9c66ab8b
Dezhin Anton <dezhin@email.com>
2023-03-14 11:04:29 +0300

    Task6 Done

---
+++ Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n %2 != 0)
+    Console.WriteLine("Нечётное");
+else
+    Console.WriteLine("Чётное");
