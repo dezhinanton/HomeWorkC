14f8bbdb1864352780bdb44fa8d4810e5ccbc58b
Dezhin Anton <dezhin@email.com>
2023-03-21 12:39:10 +0300

    Task19 Done

---
+++ Task19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string s = Console.ReadLine()!;
+while(s.Length != 5)
+{
+  Console.Write("ОШИБКА! Введите пятизначное число: ");
+  s = Console.ReadLine()!;
+}
+if (s[0] == s[4] && s[1] == s[3])
+  Console.WriteLine("Данное число - палиандром");
+else
+  Console.WriteLine("Данное число - не палиандром");
