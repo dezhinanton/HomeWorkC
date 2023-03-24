7ef413d891c7c441d1e9d7fabeab41b114aae966
Dezhin Anton <dezhin@email.com>
2023-03-24 12:35:20 +0300

    Task29 Done

---
+++ Task29/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 11); 
+Console.WriteLine($"[{string.Join(", ", array)}]");
