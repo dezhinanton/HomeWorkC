4af63093d4b59b2dbc33a93190b96ee466905ba4
Dezhin Anton <dezhin@email.com>
2023-03-24 12:23:22 +0300

    Task25 Done

---
+++ Task25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine()),result = 1;
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine()); 
+for (int i = 0; i < b; i++)
+  result *= a;
+Console.WriteLine(result);
