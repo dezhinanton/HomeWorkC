30726763ae6963aecc73daf77a9df592da9c7b1c
Dezhin Anton <dezhin@email.com>
2023-03-27 21:43:02 +0300

    Task34 Done

---
+++ Task34/Program.cs
@@ -0,0 +1,20 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 1000);
+}
+
+int SearchQuantityEven(int[] array)
+{
+    int result=0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i]%2==0) result++;
+    return result;
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.Write($"[{string.Join(", ", array)}]");
+Console.WriteLine($" -> {SearchQuantityEven(array)}");
