97932279b1854d99f57adec83945f97a78e41074
Dezhin Anton <dezhin@email.com>
2023-03-27 21:51:49 +0300

    Task26 Done

---
+++ Task36/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 100);
+}
+
+int SumOdd_i(int[] array)
+{
+    int sum=0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum=sum+array[i];
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.Write($"[{string.Join(", ", array)}]");
+Console.WriteLine($" -> {SumOdd_i(array)}");
