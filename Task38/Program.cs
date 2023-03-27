630331caeb067e7bf0ade0452d48cdd3ba8be5b3
Dezhin Anton <dezhin@email.com>
2023-03-27 22:14:14 +0300

    Task38 Done

---
+++ Task38/Program.cs
@@ -0,0 +1,25 @@
+﻿void InputArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Math.Round(new Random().NextDouble() * (100 - 10) + 10, 2);
+}
+
+double DiffMaxMin(double[] array)
+{
+    double min=array[0];
+    double max=array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]<min) min=array[i];
+        if (array[i]>max) max=array[i];
+    }
+    return max-min;
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов в массиве: ");
+int n = int.Parse(Console.ReadLine()!);
+double[] array = new double[n];
+InputArray(array);
+Console.Write($"[{string.Join("; ", array)}]");
+Console.WriteLine($" -> {DiffMaxMin(array)}");
