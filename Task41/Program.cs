693dacbb8ae71f18f79e87eddb245178a7295f31
Dezhin Anton <dezhin@email.com>
2023-03-30 15:09:04 +0300

    Task41 Done

---
+++ Task41/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int m = 5, count=0;
+Console.WriteLine($"Введите {m} чисел");
+for (int i = 0; i < m; i++)
+    if (int.Parse(Console.ReadLine()!)>0) count++;
+Console.WriteLine($"Пользователь ввёл {count} положительных чисел");
