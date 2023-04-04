aa233752c4e7c06128b9909f213e1e28cc5ca055
Dezhin Anton <dezhin@email.com>
2023-04-04 11:58:13 +0300

    Task50 Done

---
+++ Task50/Program.cs
@@ -0,0 +1,30 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine("Введите позицию элемента: ");
+int[] pos = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+if (pos[0]>size[0]||pos[1]>size[1])
+    Console.Write("Такой позиции в массиве нет");
+else Console.Write(matrix[pos[0],pos[1]]);
