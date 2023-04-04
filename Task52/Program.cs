dbf197984b2d9a6fcf897b52c7150c0c34fa6fc3
Dezhin Anton <dezhin@email.com>
2023-04-04 12:02:22 +0300

    Task52 Done

---
+++ Task52/Program.cs
@@ -0,0 +1,37 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 11); 
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+void Average(int[,] matrix)
+{
+    double avrg=0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(0); j++)
+            avrg=avrg+matrix[j,i];
+        avrg=avrg/matrix.GetLength(0);
+        Console.WriteLine($"Среднее арифметическое столбца {i}: {Math.Round(avrg, 2)}");
+        avrg=0;
+    }
+}
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Average(matrix);
