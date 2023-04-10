a32f152dd98e378a06522efa1c008ca31d1332e0
Dezhin Anton <dezhin@email.com>
2023-04-10 13:40:11 +0300

    Task66 Done

---
+++ Task66/Program.cs
@@ -0,0 +1,12 @@
+﻿int f(int m, int n)
+{
+  if (n == m)
+    return m;
+  return f(m + 1, n) + m;
+}
+
+Console.Write("Введите начальное значение: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное значение: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
